ad37b2ec0b8283440865014605d25b081d41f5eb
JeseungHwang <hjs0790@gmail.com>
2018-11-30 16:09:35 +0900

    port change

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
-var express = require('express');
+﻿var express = require('express');
 var path = require('path');
 var app = express();				        //서버 생성
 var bodyParser = require('body-parser');	
 var app = express();
 var http = require('http');
 //var fs = require("fs");
+const morgan = require('morgan');
 
 
 
 app.use(express.static(path.join( __dirname + '/public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(morgan('default'));
 
 
 var router = require('./router/main')(app);
 
-var server = app.listen(7878, function(){ 
+var server = app.listen(3000, '0.0.0.0', function(){ 
     console.log('Server is running...');
 });
