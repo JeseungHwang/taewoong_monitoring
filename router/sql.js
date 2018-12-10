9a6e02c86195d3462677b43a4f6b10104e7952eb
JeseungHwang <hjs0790@gmail.com>
2018-12-10 18:14:36 +0900

    fix

--- router/sql.js
+++ router/sql.js
@@ -3,8 +3,30 @@ module.exports = function(app, fs, mysql, connection, moment)
     var moment = require('moment');
     var gasID = ['1237002153297','53897612946769','59395171085649','68191264107857','44002008296785'];
     
-    app.get('/Gas/:gasNumber', function(req, res){
-      connection.query('SELECT * from iiot WHERE id = ? AND name = "보정적산량" ORDER BY createdAt DESC LIMIT 121',gasID[req.params.gasNumber-1], function(err,result,rows){
+    app.get('/Gas/:heatingFurnaceNumber', function(req, res){
+      connection.query('SELECT * from iiot WHERE id = ? AND name = "보정적산량" ORDER BY createdAt DESC LIMIT 121',gasID[req.params.heatingFurnaceNumber-1], function(err,result,rows){
+          if (err){
+              res.send('Select query Error [Code : 0001]');
+              return;
+            }
+            result = setHHMMformat(result);
+            res.send(result)
+        });
+    });
+
+    app.get('/Electricity/:idNumber', function(req, res){
+      connection.query('SELECT * from iiot WHERE id = ? ORDER BY createdAt DESC LIMIT 121', req.params.idNumber, function(err,result,rows){
+          if (err){
+              res.send('Select query Error [Code : 0001]');
+              return;
+            }
+            result = setHHMMformat(result);
+            res.send(result)
+        });
+    });
+
+    app.get('/Temperature/:idNumber1', function(req, res){
+      connection.query('SELECT * from iiot WHERE id = ? ORDER BY createdAt DESC LIMIT 121', req.params.idNumber1, function(err,result,rows){
           if (err){
               res.send('Select query Error [Code : 0001]');
               return;
