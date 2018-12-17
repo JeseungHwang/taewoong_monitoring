2bda75c1456899770a514548caded63d7f6d3b3c
JeseungHwang <hjs0790@gmail.com>
2018-12-17 17:54:08 +0900

    main fix

--- router/sql.js
+++ router/sql.js
@@ -1,10 +1,18 @@
 module.exports = function(app, fs, mysql, connection, moment)
 {
     var moment = require('moment');
+    var heatingFurnaceID=[
+      {'name' : '1호기', 'gas' : '1237002153297', 'elect' : '1', 'temper1' : '6', 'temper2' : ''},
+      {'name' : '2호기', 'gas' : '53897612946769', 'elect' : '2', 'temper1' : '7', 'temper2' : '8'},
+      {'name' : '3호기', 'gas' : '59395171085649', 'elect' : '3', 'temper1' : '9', 'temper2' : '10'},
+      {'name' : '4호기', 'gas' : '68191264107857', 'elect' : '4', 'temper1' : '11', 'temper2' : '12'},
+      {'name' : '5호기', 'gas' : '44002008296785', 'elect' : '5', 'temper1' : '13', 'temper2' : '14'},
+    ];
     var gasID = ['1237002153297','53897612946769','59395171085649','68191264107857','44002008296785'];
+
     
     app.get('/Gas/:heatingFurnaceNumber', function(req, res){
-      connection.query('SELECT * from iiot WHERE id = ? AND name = "보정적산량" ORDER BY createdAt DESC LIMIT 121',gasID[req.params.heatingFurnaceNumber-1], function(err,result,rows){
+      connection.query('SELECT * from iiot WHERE id = ? AND name = "보정적산량" ORDER BY createdAt DESC LIMIT 121',heatingFurnaceID[req.params.heatingFurnaceNumber-1].gas, function(err,result,rows){
           if (err){
               res.send('Select query Error [Code : 0001]');
               return;
@@ -15,7 +23,7 @@ module.exports = function(app, fs, mysql, connection, moment)
     });
 
     app.get('/Electricity/:idNumber', function(req, res){
-      connection.query('SELECT * from iiot WHERE id = ? ORDER BY createdAt DESC LIMIT 121', req.params.idNumber, function(err,result,rows){
+      connection.query('SELECT * from iiot WHERE id = ? ORDER BY createdAt DESC LIMIT 121', heatingFurnaceID[req.params.idNumber-1].elect, function(err,result,rows){
           if (err){
               res.send('Select query Error [Code : 0001]');
               return;
@@ -25,8 +33,8 @@ module.exports = function(app, fs, mysql, connection, moment)
         });
     });
 
-    app.get('/Temperature/:idNumber1', function(req, res){
-      connection.query('SELECT * from iiot WHERE id = ? ORDER BY createdAt DESC LIMIT 121', req.params.idNumber1, function(err,result,rows){
+    app.get('/Temperature/:idNumber', function(req, res){
+      connection.query('SELECT * from iiot WHERE id = ? ORDER BY createdAt DESC LIMIT 121', heatingFurnaceID[req.params.idNumber-1].temper1, function(err,result,rows){
           if (err){
               res.send('Select query Error [Code : 0001]');
               return;
